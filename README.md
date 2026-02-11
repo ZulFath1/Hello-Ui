f4008084f008b4b646b6f6f8b549fe38fde53f3c
Muhammad Dzul Fathi Ahyan <2410817210011@msh.ulm.ac.id>
2026-02-11 10:27:44 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# HelloUI-PemrogramanMobile
+## Tugas 1 Pemrograman Mobile 
+Nama : Muhammad Dzul Fathi Ahyan <br>
+NIM  : 2410817210011 <br>
+### Deskripsi  
+Aplikasi ini merupakan aplikasi dengan menggunakan bahasa pemrograman kotlin yang dilengkapi dengan fitur input nama dan akan menghasilkan output berupa "Halo, nama!" yang juga dilengkapi mode gelap dan terang<br>
+### Cara Menjalankan
+Cara Menjalankan : 
+1. Clone repository ini menggunakan terminal atau CMD
+   ```
+   git clone [https://github.com/ZulFath1/Hello-Ui.git]
+3. Buka folder repository yang telah diclone dengan Android Studio
+4. Tunggu proses Gradle Sync selesai.
+5. Lakukan klik Run pada bagian atas atau menggunakan tombol Shift+F10
+6. Aplikasi berhasil dijalankan menggunakan emulator bawaan dari Android Studio <br>
+
+### Gambar
+
+<p align="center">
+  <img src="ApplicationScreenshoot/1.png" width="300" />
+  <img src="ApplicationScreenshoot/2.png" width="300" />
+  <img src="ApplicationScreenshoot/3.png" width="300" />
+  <img src="ApplicationScreenshoot/4.png" width="300" />
+</p>
---
+++ app/src/main/java/com/example/tugas1/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.example.tugas1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.tugas1.ui.theme.Tugas1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            var isDarkTheme by remember {
+                mutableStateOf( value = false)
+            }
+
+            Tugas1Theme( darkTheme = isDarkTheme) {
+                Scaffold( modifier = Modifier.fillMaxSize()) {innerPadding ->
+                    UiApp(
+                        modifier = Modifier.padding( innerPadding ),
+                        isDark = isDarkTheme,
+                        onThemeChange = { isDarkTheme = it }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun UiApp(modifier : Modifier = Modifier, isDark: Boolean, onThemeChange: (Boolean) -> Unit) {
+
+    var name by remember { mutableStateOf(value = "") }
+
+    var greetingResult by remember { mutableStateOf(value = "") }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        OutlinedTextField(
+            value = name,
+            onValueChange = { name = it },
+            label = { Text(text = "Masukkan Nama") },
+            modifier = modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                if (name.isEmpty()) {
+                    greetingResult = "Hello Mok!"
+                } else {
+                    greetingResult = "Hello $name!"
+                }
+            }
+        ) {
+            Text("Greeting")
+        }
+        Spacer(modifier = Modifier.height(16.dp)) // Jarak kosong
+
+        Text(
+            text = greetingResult,
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(text = if (isDark) "Dark Mode" else "Light Mode")
+            Spacer(modifier = Modifier.width(8.dp))
+            Switch(
+                checked = isDark,
+                onCheckedChange = { onThemeChange(it) } // Mengubah tema saat digeser
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Tugas1Theme {
+        UiApp(isDark = true, onThemeChange = {})
+    }
+}
